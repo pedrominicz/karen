3d9459771ba46279b9b20fa8f999ddd31b8b792a
Pedro Minicz <pedrominicz@gmail.com>
2019-12-26 10:54:26 -0300

    Implement full unification.

--- Karen.hs
+++ Karen.hs
@@ -1,32 +1,71 @@
-module Karen ((===), atom, conj, disj, empty, fresh) where
+module Karen ((===), atom, conj, disj, empty, fresh, term) where
 
 -- https://github.com/seantalts/hasktrip/blob/master/doc/MicroKanren.md
 -- http://webyrd.net/scheme-2013/papers/HemannMuKanren2013.pdf
 
+import Control.Monad.State
+import Data.IntMap
+
+import Prelude hiding (lookup)
+
 data Term
     = Var Int
-    | Term String
+    | Term String [Term]
     deriving (Eq, Show)
 
 atom :: String -> Term
-atom x = Term x
+atom t = Term t []
 
-type Substitution = [(Int, Term)]
+term :: String -> [Term] -> Term
+term = Term
 
-walk :: Substitution -> Term -> Term
-walk s (Var v) =
+type Substitution = IntMap Term
+
+type Unify a = StateT Substitution Maybe a
+
+bind :: Int -> Term -> Unify Term
+bind v t = do
+    modify $ insert v t
+    return t
+
+occurs :: Int -> Term -> Bool
+occurs v (Term t ts) = any (occurs v) ts
+occurs v (Var v')    = v == v'
+
+apply :: Term -> Unify Term
+apply (Term t ts) = Term t <$> traverse apply ts
+apply (Var v) = do
+    s <- get
     case lookup v s of
-        Just x  -> x
-        Nothing -> Var v
-walk s x = x
+        Just t -> do
+            guard $ not (occurs v t)
+            t <- apply t
+            bind v t
+        Nothing -> return $ Var v
 
 unify :: Substitution -> Term -> Term -> Maybe Substitution
-unify s x y = (++ s) <$> go (walk s x) (walk s y)
+unify s t t' = execStateT (go t t') s
     where
-    go x y | x == y = Just []
-    go (Var v) x = Just [(v, x)]
-    go x (Var v) = Just [(v, x)]
-    go _ _ = Nothing
+    go :: Term -> Term -> Unify Term
+    go t t' = do
+        t  <- apply t
+        t' <- apply t'
+        case (t, t') of
+            (t, t') | t == t' -> return t
+            (Var v, t) -> bind v t
+            (t, Var v) -> bind v t
+            (t, t') -> match t t'
+
+    match (Term t ts) (Term t' ts') = do
+        guard (t == t')
+        ts <- zip ts ts'
+        ts <- traverse (uncurry go) ts
+        return $ Term t ts
+    match t t' = go t t'
+
+    zip [] []         = return []
+    zip (x:xs) (y:ys) = ((x, y) :) <$> zip xs ys
+    zip _ _           = mzero
 
 type Goal = (Substitution, Int) -> [(Substitution, Int)]
 
@@ -34,8 +73,8 @@ fresh :: (Term -> Goal) -> Goal
 fresh f = \(s, i) -> f (Var i) (s, i + 1)
 
 (===) :: Term -> Term -> Goal
-x === y = \(s, i) ->
-    case unify s x y of
+t === t' = \(s, i) ->
+    case unify s t t' of
         Just s  -> [(s, i)]
         Nothing -> []
 
@@ -46,18 +85,15 @@ disj f g = \x -> f x ++ g x
 conj :: Goal -> Goal -> Goal
 conj f g = \x -> f x >>= g
 
-empty :: (Substitution, Int)
-empty = ([], 0)
-
 -- zero = atom "0"
 -- one = atom "1"
 -- two = atom "2"
--- conj (fresh $ \x -> x === zero) (fresh $ \y -> disj (y === one) (y === two)) empty
+-- conj (fresh $ \x -> x === zero) (fresh $ \y -> disj (y === one) (y === two)) (empty, 0)
 -- zeros x = disj (x === zero) (zeros x)
 -- ones x = disj (x === one) (ones x)
--- take 4 $ fresh (\x -> disj (zeros x) (ones x)) empty
+-- take 4 $ fresh (\x -> disj (zeros x) (ones x)) (empty, 0)
 -- a = atom "a"
 -- b = atom "b"
 -- c = atom "c"
 -- magic = fresh (\x -> disj (x === a) (disj (x === b) (x === c)))
--- magic empty
+-- magic (empty, 0)
